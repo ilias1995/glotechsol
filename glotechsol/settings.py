d6e872345142ddd373f87d4f9b76f243a2e48351
kazikhodzhaeviliyaz <iliyaz@glotechsol.com>
2016-12-20 14:23:07 +0600

    static files problem

--- glotechsol/settings.py
+++ glotechsol/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'q@4_k4+n#xiy++8$(r33h3z89n6yi(35db99v2d6wd_h)x*x+j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['148.251.8.245']
+ALLOWED_HOSTS = ['*']
 
 GEOIP_PATH = join(BASE_DIR, 'geolocation')
 
